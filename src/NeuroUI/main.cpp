b72506f2748e8044f0973e2de6c1954df40374b9
Namilskyy <alive6863@gmail.com>
2025-02-10 23:23:51 +0300

    Main commit

---
+++ src/NeuroUI/main.cpp
@@ -0,0 +1,14 @@
+#include "./ui_main.h"
+#include "mw.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
