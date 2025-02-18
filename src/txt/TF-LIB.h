00e20d8186e741c6eb788d28316b02ce7d50b80b
ArcaneDev <alive6863@gmail.com>
2025-02-18 16:06:15 +0300

    Update TF-LIB.h

--- src/txt/TF-LIB.h
+++ src/txt/TF-LIB.h
@@ -1,5 +1,16 @@
-#ifndef TDF_LIB_H
-#define TDF_LIB_H 
+/*
+
+  _______                                ________              __________
+  ___    |__________________ _______________  __ \_______   __ ___  ___  \
+  __  /| |_  ___/  ___/  __ `/_  __ \  _ \_  / / /  _ \_ | / / __  / _ \  |
+  _  ___ |  /   / /__ / /_/ /_  / / /  __/  /_/ //  __/_ |/ /  _  / , _/ /
+  /_/  |_/_/    \___/ \__,_/ /_/ /_/\___//_____/ \___/_____/   |_/_/|_|_/
+                                                              
+
+*/
+
+#ifndef TF_LIB_H
+#define TF_LIB_H 
 
 #include <stdio.h>
 
@@ -18,5 +29,15 @@ typedef struct {
 void calculate_tf(Document *doc, const char *text);
 void calculate_idf();
 void calculate_tfidf();
+
+/*
+  `calculate_idf()` and `calculate_tfidf()` have not args, now to compute 
+  TF-IDF you need use ALL this funcs in order
+
+ 'calculate_idf()' и 'calculate_tfidf()' не имеют аргументов, 
+ сейчас для вычисления TF-IDF вам нужно использовать ВСЕ эти функции для того, по порядку
+
+*/
+
 
 #endif
